ad7637c3bc348e0884eb94fdc2eaf213d3e6749b
Sebastian Cachis <u202210846@upc.edu.pe>
2025-01-15 16:10:26 -0500

    feat(navbar): add close button for improved navigation Added a close button to the navbar for enhanced user experience and better navigation control.

--- scripts/script.js
+++ scripts/script.js
@@ -9,14 +9,24 @@ const menuOverlay = document.createElement('div');
 menuOverlay.classList.add('menu-overlay');
 document.body.appendChild(menuOverlay);
 
+// Element for the close button
+const closeMenu = document.getElementById('close-menu');
+
+// Element for the language switcher
+const languageSwitcher = document.querySelector('.change-language-container');
+
 // Event listener to toggle the 'active' class on the navigation container and overlay when the mobile menu is clicked
 mobileMenu.addEventListener('click', () => {
     navContainer.classList.toggle('active');
     menuOverlay.classList.toggle('active');
+    document.body.classList.toggle('menu-active'); // Add class to body
+    languageSwitcher.classList.toggle('disabled'); // Disable language switcher
 });
 
-// Event listener to close the menu when the overlay is clicked
-menuOverlay.addEventListener('click', () => {
+// Event listener to close the menu when the close button is clicked
+closeMenu.addEventListener('click', () => {
     navContainer.classList.remove('active');
     menuOverlay.classList.remove('active');
+    document.body.classList.remove('menu-active'); // Remove class from body
+    languageSwitcher.classList.remove('disabled'); // Enable language switcher
 });
